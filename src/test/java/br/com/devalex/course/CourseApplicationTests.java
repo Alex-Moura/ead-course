4aceee5ba5c6fd273823c69959a17a100f92042c
Alex Moura <alexxmooura322@gmail.com>
2026-04-22 21:23:19 -0300

    first commit

---
+++ src/test/java/br/com/devalex/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devalex.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
