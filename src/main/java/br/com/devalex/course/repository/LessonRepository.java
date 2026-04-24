cf0b3eab4d83ec71762acd3bf4c2e024cd76cf4b
Alex Moura <alexxmooura322@gmail.com>
2026-04-24 07:49:44 -0300

    adiciona pacote repository para Course,Module e Lesson

---
+++ src/main/java/br/com/devalex/course/repository/LessonRepository.java
@@ -0,0 +1,11 @@
+package br.com.devalex.course.repository;
+
+import br.com.devalex.course.model.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, UUID> {
+}
