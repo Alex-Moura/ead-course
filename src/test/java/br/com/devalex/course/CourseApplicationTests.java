89040a4050f66fc2c2f82fb0ce242ec33abf9fa2
Alex Moura <alexxmooura322@gmail.com>
2026-06-04 22:46:00 -0300

    refactor: alguns ajustes

--- src/test/java/br/com/devalex/course/CourseApplicationTests.java
+++ src/test/java/br/com/devalex/course/CourseApplicationTests.java
@@ -1,21 +1,13 @@
 package br.com.devalex.course;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class CourseApplicationTests {
 
-	@Autowired
-	private ApplicationContext context;
-
 	@Test
 	void contextLoads() {
-		assertThat(context).isNotNull();
 	}
 
 }
